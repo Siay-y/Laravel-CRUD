5d50f6d4e05dc81594379e06a4e0e77ae3db3671
Luiz Santos <luiz.porfiriosantos@gmail.com>
2026-03-26 01:01:59 -0300

    [hotfix-0001] Refatora Sistema de Rotas.

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [Homecontroller::class, 'index'])->name('home');
 
-// Users - Views
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-
-// Users - Actions
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+// Usuário
+Route::resource('users', UserController::class);
